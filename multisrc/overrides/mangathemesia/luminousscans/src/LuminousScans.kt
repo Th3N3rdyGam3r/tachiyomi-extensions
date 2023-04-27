458b4807772a0bfa6394c635a917a54325c5ff78
Th3N3rdyGam3r <Th3N3rdyGam3r@users.noreply.github.com>
2023-04-27 19:54:51 +0000

    Delete multisrc/overrides/mangathemesia directory

--- multisrc/overrides/mangathemesia/luminousscans/src/LuminousScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.luminousscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class LuminousScans : MangaThemesia("Luminous Scans", "https://luminousscans.com", "en", mangaUrlDirectory = "/series") {
-    override val pageSelector = "div#readerarea > :not(.swiper) img"
-}
